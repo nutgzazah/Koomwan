2511055ec0f4e5b177353a4d32d8a3841b78373b
thidaruttd97gooddaygoodlife <thiz.codingUUtd97@gmail.com>
2025-01-15 23:54:14 +0700

    Adding Custom Styles From Our Design System ➕
    By Using Utility Classes from Tailwind CSS .

--- koomwan/App.tsx
+++ koomwan/App.tsx
@@ -1,13 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import * as Font from 'expo-font';
+import { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
-import "./global.css"
+import "./global.css"; 
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'K2D-Regular': require('./assets/fonts/K2D-Regular.ttf'),
+        'K2D-Medium': require('./assets/fonts/K2D-Medium.ttf'),
+        'K2D-Bold': require('./assets/fonts/K2D-Bold.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <Text>Loading Fonts...</Text>;
+  }
+
   return (
-    <View className='flex-1 content-center justify-center bg-red-500'>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+//Test
+    <View className="bg-background p-14">
+      <Text className="text-title font-sans">Test K2D Title คุมหวาoน</Text>
+      <Text className="text-display font-sans">Test K2D Display คุมหวาน</Text>
+      <Text className="text-sub-button font-sans">Test K2D Body คุมหวาน</Text>
     </View>
   );
 }
-
--- koomwan/tailwind.config.js
+++ koomwan/tailwind.config.js
@@ -1,10 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app.{js,jsx,ts,tsx}"],
+  content: [
+    "./app.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3972F0",
+        secondary: "#3E3B5B",
+        background: "#F8F8F8",
+        card: "#FFFFFF",
+        normal: "#2ED74D",
+        warning: "#FFD444",
+        abnormal: "#FE5757", // For Unsuccess Status And Risk Status
+        unread: "#D6E3FF",
+      },
+      fontFamily: {
+        sans: ["K2D-Regular", "K2D-Medium", "K2D-Bold"],
+      },
+      fontSize: {
+        display: ["28px", { lineHeight: "1.75rem", fontWeight: "700" }],
+        title: ["24px", { lineHeight: "1.5rem", fontWeight: "700" }],
+        headline: ["20px", { lineHeight: "1.125rem", fontWeight: "500" }],
+        description: ["14px", { lineHeight: "0.875rem", fontWeight: "400" }],
+        tag: ["12px", { lineHeight: "0.75rem", fontWeight: "500" }],
+        button: ["14px", { lineHeight: "0.875rem", fontWeight: "700" }],
+        "sub-button": ["12px", { lineHeight: "0.875rem", fontWeight: "700" }],
+      },
+    },
   },
   plugins: [],
-}
+};
