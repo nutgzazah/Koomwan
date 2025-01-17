edeeae3182cd6532a85b50e5aebf0e98f8f22783
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2025-01-17 18:31:47 +0700

    Add BreakLine Components

--- koomwan/app/(tabs)/resource.tsx
+++ koomwan/app/(tabs)/resource.tsx
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
-import Card, { BreakLine } from "../../global/components/Card";
+import Card from "../../global/components/Card";
+import BreakLine from "../../global/components/BreakLine";
 
 const exampleIcon = require("../../assets/BeginnerSetup/cake.png") // ลบทิ้งตอนทำจริง
 
