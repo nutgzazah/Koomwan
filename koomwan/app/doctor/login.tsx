a2c04b797ef44100022e393c751fe4e9abed453d
phuparu <phumiphat.140@gmail.com>
2025-01-20 01:46:39 +0700

    Using LoginLayout

--- koomwan/app/doctor/login.tsx
+++ koomwan/app/doctor/login.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
+import LoginLayout from "../../components/login_signin/LoginLayout";
 
 export default function DoctorLoginScreen() {
+  const router = useRouter();
+
+  const handleLogin = (username: string, password: string) => {
+    // Handle login logic
+  };
+
   return (
-    <View>
-      <Text>DoctorLoginScreen</Text>
-    </View>
+    <LoginLayout
+      title="เข้าสู่ระบบ"
+      description="บุคลากรทางการแพทย์"
+      backgroundImage={require("../../assets/Login/images/doctor_login.png")}
+      onForgotPassword={() => router.push("/user/forgotpass")}
+      onRegister={() => router.push("/doctor/signin")}
+      onLogin={handleLogin}
+    />
   );
 }
