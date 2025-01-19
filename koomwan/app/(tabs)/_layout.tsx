38dd86d48aad00e6ec2567473055900f7ca3cd60
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2025-01-19 10:57:33 +0700

    Update Navbar

--- koomwan/app/(tabs)/_layout.tsx
+++ koomwan/app/(tabs)/_layout.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image
+} from "react-native";
 import React from "react";
-import { RelativePathString, Tabs } from "expo-router";
+import { useRouter, Tabs } from "expo-router";
 import { NavTabIcon, NavBigIcon } from "../../global/components/NavBarBottom";
 
 export default function TabsLayout() {
+  const router = useRouter();
 
   // โค๊ดส่วนนี้ Import รูปภาพจาก Assets สำหรับ Navbar
+  const profileIcon = require("../../assets/Navbar/mock-profile.png");
+  const logoIcon = require("../../assets/Navbar/Logo.png");
+  const notificationsIcon = require("../../assets/Navbar/notification.png");
   const homeIcon = require("../../assets/Navbar/home.png");
   const homeBoldIcon = require("../../assets/Navbar/home-bold.png");
   const suggestionIcon = require("../../assets/Navbar/weight.png");
@@ -16,13 +25,56 @@ export default function TabsLayout() {
   const forumIcon = require("../../assets/Navbar/messages.png");
   const forumBoldIcon = require("../../assets/Navbar/messages-bold.png");
 
-  // Path ที่ไม่ต้องการให้แสดงอยู่ในแท็บอย่างเช่น notification, profile, setting
-  const hiddenPaths = ["notification"];
+  // Route ที่ไม่ต้องการให้แสดงอยู่ในแท็บอย่างเช่น notification, profile, setting
+  // ตอนนี้ใส่คอมเมนต์ไปเพราะยังไม่ได้ทำ route, ถอดคอมเมนต์ข้างล่างออกได้ตอนทำ route จริง
+  // const hiddenRoutes = [];
 
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
+        headerLeft: () => (
+          // headerLeft : ส่วน Icon โปรไฟล์
+          // โปรไฟล์ แก้ไข Path รูปโปรไฟล์ ตอนทำจริง
+          // บทบาท (ทั่วไป) แก้ไขตอนทำจริง
+          <TouchableOpacity 
+            onPress={(() => router.push("suggestion"))}
+            className="ml-4"
+          >
+            <View className="flex-row items-baseline">
+              <Image className="w-12 h-12"
+                source={profileIcon}
+              />
+              <View className="bg-primary right-6 px-3 rounded-3xl">
+                <Text className="text-card font-sans font-medium">ทั่วไป</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ),
+        // headerTitle : ส่วน Logo
+        headerTitle: () => (
+          <TouchableOpacity 
+            onPress={(() => router.replace(""))}
+
+          >
+            <Image className="w-10 h-10"
+              source={logoIcon}
+            />
+          </TouchableOpacity>
+        ),
+        // headerRight : ส่วน Icon notification
+        // notification แก้ไข Path ตอนทำจริง
+        headerRight: () => (
+          <TouchableOpacity 
+            onPress={(() => router.push("resource"))}
+            className="mr-4"
+          >
+            <Image className="w-6 h-6"
+              source={notificationsIcon}
+            />
+          </TouchableOpacity>
+        ),
+        headerTitleAlign: "center",
         tabBarShowLabel: false,
         tabBarStyle: {
           position: "absolute",
@@ -95,16 +147,18 @@ export default function TabsLayout() {
           )
         }}
       />
-      { // อันนี้เอาไว้ซ่อน Path ที่ไม่ต้องให้อยู่บน Navbar ข้างล่าง
-        hiddenPaths.map((path, key) => (
-          <Tabs.Screen
-            key={key}
-            name={path}
-            options={{
-              href: null,
-            }}
-          />
-      ))}
+      { // อันนี้เอาไว้ซ่อน Route ที่ไม่ต้องให้อยู่บน Navbar ข้างล่าง 
+        // คอมเมนต์เอาไว้ก่อน ตอนมีค่อยถอดคอมเมนต์ข้างล่าง
+        // hiddenRoutes.map((path, key) => (
+        //   <Tabs.Screen
+        //     key={key}
+        //     name={path}
+        //     options={{
+        //       href: null,
+        //     }}
+        //   />
+        // ))
+      }
     </Tabs>
   );
 }
--- koomwan/app/_layout.tsx
+++ koomwan/app/_layout.tsx
@@ -1,47 +1,18 @@
-import {
-  Stack,
-  useRouter,
-  RelativePathString
-}from "expo-router";
+import { Stack, } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import "../global.css";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import React from "react";
-import { TouchableOpacity, Image, View, Text } from "react-native";
-import { ImageSourcePropType } from 'react-native';
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const router = useRouter();
   const [fontsLoaded] = useFonts({
     "K2D-Bold": require("../assets/fonts/K2D-Bold.ttf"),
     "K2D-Regular": require("../assets/fonts/K2D-Regular.ttf"),
     "K2D-Medium": require("../assets/fonts/K2D-Medium.ttf"),
   });
-
-  interface navTopConfig {
-    targetRoute: string, // Stack.Screen name attribute
-    profileIconPath: ImageSourcePropType, // รูปโปรไฟล์
-    role: string, // บทบาทผู้ใช้
-    profileRoute: RelativePathString, // path ไปหน้าโปรไฟล์
-    logoIconPath: ImageSourcePropType, // โลโก้แอพ
-    homeRoute: RelativePathString, // path ไปหน้าโฮม
-    notificationsIconPath: ImageSourcePropType, // Icon แจ้งเตือน
-    notificationsRoute: RelativePathString, // path ไปหน้าแจ้งเตือน 
-  }
-
-  const config: navTopConfig = {
-    targetRoute: "(tabs)",
-    profileIconPath: require("../assets/Navbar/mock-profile.png"),
-    role: "ทั่วไป",
-    profileRoute: "../(tabs)/forum", // แก้ไขตอนทำหน้าจริง
-    logoIconPath: require("../assets/Navbar/Logo.png"),
-    homeRoute: "../(tabs)",
-    notificationsIconPath: require("../assets/Navbar/notification.png"),
-    notificationsRoute: "../(tabs)/resource", // แก้ไขตอนทำหน้าจริง
-  }
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -56,46 +27,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="onboarding/index" />
 
-      {/* 
-        ข้างล่างนี้เป็น NavBarTop 
-        ประสิทธิภาพยังไม่ค่อยดี ตอนนี้กดบนเครื่องจริงแล้วช้า
-      */}
-      <Stack.Screen
-        name={config.targetRoute}
-        options={{
-          headerShown: true,
-          headerLeft: () => (
-            <TouchableOpacity onPress={(() => router.push(config.profileRoute))}>
-              <View className="flex-row items-baseline">
-                <Image
-                  source={config.profileIconPath}
-                  style={{ width: 48, height: 48, marginTop: 14, marginBottom: 13 }}
-                />
-                <View className="bg-primary right-6 px-3 rounded-3xl">
-                  <Text className="text-card font-sans font-medium">{config.role}</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          ),
-          headerTitle: () => (
-            <TouchableOpacity onPress={(() => router.replace(config.homeRoute))}>
-              <Image
-                source={config.logoIconPath}
-                style={{ width: 40, height: 40 }}
-              />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity onPress={(() => router.push(config.notificationsRoute))}>
-              <Image
-                source={config.notificationsIconPath}
-                style={{ width: 24, height: 24 }}
-              />
-            </TouchableOpacity>
-          ),
-          headerTitleAlign: "center",
-        }}
-      />
+      <Stack.Screen name="(tabs)" />
 
       <Stack.Screen name="profile/index" />
 
