e0eb826566ea7afbfd2025e971e723fb5c2acb61
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2025-01-20 12:51:10 +0700

    Add Resource page

---
+++ koomwan/app/(tabs)/resource/components/ArticleBox.tsx
@@ -0,0 +1,52 @@
+import {
+    View,
+    Text,
+    Image,
+    ImageSourcePropType,
+    TouchableOpacity,
+} from "react-native";
+import React from "react";
+import CategoryBox from "./CategoryBox";
+import Card from "../../../../global/components/Card";
+import { useRouter } from "expo-router";
+
+export interface articleBoxProps {
+    title: string,
+    imageSource: ImageSourcePropType,
+    author: string,
+    categories: Array<string>
+}
+
+export default function ArticleBox(
+    { title, imageSource, author, categories }: articleBoxProps
+) {
+    const router = useRouter()
+
+    return (
+        <TouchableOpacity onPress={(() => router.push("/resource/context", { relativeToDirectory: true }))}>
+            <Card>
+                <View className="mx-3 w-full h-36">
+                    <Image
+                        className="w-full h-full"
+                        source={imageSource}
+                    />
+                </View>
+                <View className="mx-3 justify-start flex w-full">
+                    <Text className="font-sans text-body text-secondary">
+                        {title}
+                    </Text>
+                    <Text className="font-sans text-tag text-secondary">
+                        เขียนโดย : {author}
+                    </Text>
+                </View>
+                <View className="justify-start mx-3 flex flex-wrap flex-row w-full">
+                    {
+                        categories.map((category, index) => (
+                            <CategoryBox key={index} category={category} />
+                        ))
+                    }
+                </View>
+            </Card>
+        </TouchableOpacity>
+    )
+}
