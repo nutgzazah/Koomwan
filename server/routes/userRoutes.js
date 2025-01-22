f10615489072d72a7015cbdb05c4c07388607899
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2025-01-23 03:00:41 +0700

    add OTP

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { registerController, loginController } = require('../controllers/userController')
+const { registerController, loginController, checkDuplicateController } = require('../controllers/userController')
 
 //router object
 const router = express.Router()
@@ -7,7 +7,7 @@ const router = express.Router()
 //routes
 // REGISTER || POST
 router.post('/register', registerController)
-
+router.post('/checkDuplicate', checkDuplicateController)
 // LOGIN || POST
 router.post('/login', loginController)
 
