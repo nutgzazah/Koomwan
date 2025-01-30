48f63bb7bf2ebaa9978332a9cb0e3e34b086d312
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2025-01-30 19:07:27 +0700

    Add notification page

--- koomwan/app/(tabs)/notification/index.tsx
+++ koomwan/app/(tabs)/notification/index.tsx
@@ -7,6 +7,9 @@ import {
   Text
 } from "react-native";
 import React, { useState } from "react";
+import Card from "../../../global/components/Card";
+import BreakLine from "../../../global/components/BreakLine";
+import NotificationCard from "./components/NotificationCard";
 
 export default function ResourceScreen() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -17,7 +20,15 @@ export default function ResourceScreen() {
         className="mb-24"
         showsVerticalScrollIndicator={false}
       >
-        <Text>Notification</Text>
+        <Card>
+            <Text className="font-sans text-title text-secondary">
+                การแจ้งเตือน
+            </Text>
+            <BreakLine/>
+            <NotificationCard/>
+            <NotificationCard/>
+            <NotificationCard/>
+        </Card>
       </ScrollView>
     </SafeAreaView>
   );
