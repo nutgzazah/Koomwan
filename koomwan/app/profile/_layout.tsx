b73bd7e8d3b547247da461973498634acb7a930e
phuparu <phumiphat.140@gmail.com>
2025-01-31 18:55:54 +0700

    Rename route

--- koomwan/app/profile/_layout.tsx
+++ koomwan/app/profile/_layout.tsx
@@ -6,7 +6,7 @@ export default function ProfileLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="editprofile" />
+      <Stack.Screen name="editProfile" />
     </Stack>
   );
 }
