f65df2ef66890c9275f2657e3ac7cc410b8d5bba
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2025-02-06 10:49:39 +0700

    Update Support System Menu
    - Update CSS and others

--- admin/src/app/supportSystem/page.tsx
+++ admin/src/app/supportSystem/page.tsx
@@ -1,13 +1,41 @@
 'use client';
 
-import React from "react";
+import React, { useState } from "react";
 import ReportTable from "./components/ReportTable";
+import reports from "../../data/report.json";
+import { ReportDataInterface } from "@/interfaces/reportInterface";
+import { statusReport } from "@/utils/statusMapping";
 
 export default function SupportSystem() {
+  const reportList = reports as ReportDataInterface[];
+  const [filterStatus, setFilterStatus] = useState<string>(statusReport.pending);
+
+  const filteredReport = reportList.filter((report) => {
+    const statusKey = Object.keys(statusReport).find(
+      (key) => statusReport[key] === filterStatus
+    );
+    return report.resolved === statusKey;
+  });
+
   return (
-    <div>
-        <ReportTable />
+    <div className="flex flex-col items-center min-h-screen w-full space-y-4">
+      
+      {/* Filter Buttons */}
+      <div className="flex w-full space-x-4">
+        {Object.values(statusReport).map((status) => (
+          <button
+            key={status}
+            onClick={() => setFilterStatus(status)}
+            className={`w-full ${
+              filterStatus === status ? "btn blue-btn long-btn" : "btn white-btn long-btn"
+            }`}
+          >
+            {status}
+          </button>
+        ))}
+      </div>
+
+      <ReportTable reports={filteredReport} />
     </div>
   );
 }
-  
