f2669caeb2f25a31d205f31f124e1ea9775e6e4c
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2025-02-11 02:11:16 +0700

    change format

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { requireSignIn } = require('../controllers/authController')
-const { beginnerSetup, addHealthRecord } = require('../controllers/userController')
+const { beginnerSetup, } = require('../controllers/userController')
+const { addHealthRecord, updateHealthRecord } = require('../controllers/trackingController')
 
 
 //router object
@@ -14,6 +15,10 @@ router.post('/beginnerSetup' /*,requireSignIn*/ , beginnerSetup)
 //TRACKING
 //ADD RECORD || POST
 router.post('/addRecord' /*,requireSignIn*/ , addHealthRecord)
+//UPDATE RECORD || PUT
+router.put('/updateRecord' /*,requireSignIn*/ , updateHealthRecord)
+
+
 
 //export
 module.exports = router
