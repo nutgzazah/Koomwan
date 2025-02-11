a737bc2c31b2588c383c04c181f2c2048969f431
thidaruttd97gooddaygoodlife <thiz.codingUUtd97@gmail.com>
2025-02-11 17:09:16 +0700

    Make Mockup Data For Suggestion
    healthData
    ecommendationData

---
+++ koomwan/app/(tabs)/suggestion/data/healthData.tsx
@@ -0,0 +1,20 @@
+//The Mockup Data For Health Suggestion
+const healthData = {
+    healthScore: 9,
+    diabetesRisk: {
+      percentage: 10,
+      level: "ต่ำมาก",
+    },
+    summary: [
+      "คุณสุขภาพโดยรวมดี น้ำหนักและส่วนสูงสมส่วน",
+      "ระดับน้ำตาลในเลือดและ A1c อยู่ในเกณฑ์ปกติ",
+      "ความดันโลหิตดีมาก",
+    ],
+    recommendation: [
+      "ดูแลสุขภาพได้ดีมาก!",
+      "รักษาพฤติกรรมการกินและออกกำลังกายแบบนี้ต่อไป",
+      "แล้วสุขภาพจะแข็งแรงขึ้นในอนาคต ✨",
+    ],
+  };
+
+export default healthData;
---
+++ koomwan/app/(tabs)/suggestion/data/recommendationData.tsx
@@ -0,0 +1,76 @@
+//The Mockup Data For Recommendation (Food, Exercise, Article)
+const recommendationData = {
+  food: [
+    {
+      title: "สลัดผลไม้ไข่ต้ม",
+      description: "+ ควบคุมน้ำหนัก",
+      imageSrc: require("../../../../assets/Suggestion/salad-egg.png"),
+      content: "สลัดผลไม้ไข่ต้มเหมาะสำหรับการควบคุมน้ำหนัก...",
+    },
+    {
+      title: "สเต็กอกไก่",
+      description: "+ เพิ่มกล้ามเนื้อ",
+      imageSrc: require("../../../../assets/Suggestion/steak.png"),
+      content: "สเต็กอกไก่เป็นแหล่งโปรตีนที่ดีสำหรับการเพิ่มกล้ามเนื้อ...",
+    },
+    {
+      title: "ไก่ผัดมะม่วง",
+      description: "+ เพิ่มกล้ามเนื้อ",
+      imageSrc: require("../../../../assets/Suggestion/chicken-stir-fried.png"),
+      content: "สเต็กอกไก่เป็นแหล่งโปรตีนที่ดีสำหรับการเพิ่มกล้ามเนื้อ...",
+    },
+  ],
+
+  exercise: [
+    {
+      title: "โยคะ",
+      description: "+ เพิ่มความยืดหยุ่น",
+      imageSrc: require("../../../../assets/Suggestion/relaxing-muscle.png"),
+      content: "โยคะช่วยเพิ่มความยืดหยุ่นและลดความเครียด...",
+    },
+    {
+      title: "ยกน้ำหนัก",
+      description: "+ เสริมสมรรถภาพ",
+      imageSrc: require("../../../../assets/Suggestion/dumble-trainning.png"),
+      content: "การวิ่งช่วยเสริมสมรรถภาพและเผาผลาญพลังงาน...",
+    },
+    {
+      title: "ยกน้ำหนัก",
+      description: "+ เสริมสมรรถภาพ",
+      imageSrc: require("../../../../assets/Suggestion/dumble-trainning.png"),
+      content: "การวิ่งช่วยเสริมสมรรถภาพและเผาผลาญพลังงาน...",
+    },
+  ],
+
+  articles: [
+    {
+      title: "นอกจากระดับน้ำตาล นี่สิ่งที่คุณต้องระวัง...",
+      author: "นายแพทย์สมใจ หมายดี",
+      category: "สุขภาพ",
+      tags: ["การออกกำลังกาย", "โภชนาการ"],
+      imageSrc: require("../../../../assets/Suggestion/sugar-measure.png"),
+      content: "นอกจากระดับน้ำตาล นี่สิ่งที่คุณต้องระวัง...",
+    },
+    {
+      title: "เบาหวาน... ยังมีอะไรที่คุณต้องรู้",
+      author: "นายแพทย์สมใจ หมายดี",
+      category: "สุขภาพ",
+      tags: ["การออกกำลังกาย", "โภชนาการ"],
+      imageSrc: require("../../../../assets/Suggestion/diabetes-knowleage.png"),
+      content: "เบาหวาน... ยังมีอะไรที่คุณต้องรู้...",
+    },
+    {
+      title: "เบาหวาน... ยังมีอะไรที่คุณต้องรู้",
+      author: "นายแพทย์สมใจ หมายดี",
+      category: "สุขภาพ",
+      tags: ["การออกกำลังกาย", "โภชนาการ"],
+      imageSrc: require("../../../../assets/Suggestion/diabetes-knowleage.png"),
+      content: "เบาหวาน... ยังมีอะไรที่คุณต้องรู้...",
+    },
+  ],
+};
+
+export default recommendationData;
+
+
+
