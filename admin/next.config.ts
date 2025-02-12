1b17cfe16b22f33d6108f402978450bf0ba36d13
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2025-02-12 21:41:10 +0700

    add login

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/medicalManagement",
+        permanent: true, // ใช้ true ถ้าต้องการ redirect แบบถาวร
+      },
+    ];
+  },
 };
 
 export default nextConfig;
