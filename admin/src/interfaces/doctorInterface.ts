42c996aaa96db535141c00366144074cf731af59
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2025-02-13 19:33:54 +0700

    fetch doctor api

--- admin/src/interfaces/doctorInterface.ts
+++ admin/src/interfaces/doctorInterface.ts
@@ -1,16 +1,18 @@
 export interface DoctorInterface {
-    doctor_id: string;
+    _id: string;
     username: string;
     email: string;
     password: string;
     phone: string;
-    first_name: string;
-    last_name: string;
+    firstname: string;
+    lastname: string;
     occupation: string;
     expert: string;
     hospital: string;
     document: string;
-    approval: 'pending' | 'approve' | 'disapprove';
-    reason?: string;
+    approval: {
+        status: 'pending' | 'approve' | 'disapprove';
+        reason?: string;
+    };
     image?: string;
 }
