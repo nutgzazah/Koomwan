cf373cb9791eaee9648682d167169887331de6d5
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2025-02-14 03:12:13 +0700

    connect front-back blog
    - get blog
    - get blog by Id

--- admin/src/app/articleManagement/[articleId]/page.tsx
+++ admin/src/app/articleManagement/[articleId]/page.tsx
@@ -1,15 +1,32 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { BlogInterface } from '@/interfaces/blogInterface';
 import Link from 'next/link';
-import blogs from '../../../data/blog.json';
+import axios from 'axios';
+import { formatDate } from '@/utils/formatDate.';
 
 const ArticleId: React.FC = () => {
   const { articleId } = useParams();
-  const blogList = blogs as BlogInterface[];
-  const blog = blogList.find((b) => b.blog_id === articleId);
+  const [blog, setBlog] = useState<BlogInterface | null>(null);
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      if (!articleId) return;
+
+      try {
+        const response = await axios.get(`http://localhost:8080/api/v1/admin/blog/${articleId}`);
+        console.log("Fetched blog data:", response.data);
+
+        setBlog(response.data.data || response.data);
+      } catch (error) {
+        console.error("Error fetching blog data:", error);
+      }
+    };
+
+    fetchBlog();
+  }, [articleId]);
 
   if (!blog) {
     return (
@@ -32,20 +49,26 @@ const ArticleId: React.FC = () => {
 
       {/* Header Section */}
       <h2 className="text-headline_3 text-secondary">{blog.title}</h2>
-      <p className="text-detail_3 text-secondary">เขียนเมื่อ {blog.publish_date}</p>
+      <p className="text-detail_3 text-secondary">เขียนเมื่อ {formatDate(blog.date)}</p>
+
 
       {/* Category */}
       <div className="flex items-center space-x-2 mb-4">
         <p className='text-detail_3 text-secondary'>หมวดหมู่ : </p>
-        {blog.category.map((cat) => (
-          <span
-            key={cat}
-            className="btn lightblue-btn p-2 rounded-md"
-          >
-            {cat}
-          </span>
-        ))}
+        {Array.isArray(blog.category) 
+          ? blog.category.map((cat) => (
+              <span key={cat} className="btn lightblue-btn p-2 rounded-md">
+                {cat}
+              </span>
+            ))
+          : (
+            <span className="btn lightblue-btn p-2 rounded-md">
+              {blog.category}
+            </span>
+          )
+        }
       </div>
+
 
       {/* Image */}
       <div className="w-full h-auto mb-6">
