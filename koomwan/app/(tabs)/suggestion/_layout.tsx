ea0c9c4a0e54f57b7e8358148ade3598d178b25b
thidaruttd97gooddaygoodlife <thiz.codingUUtd97@gmail.com>
2025-02-14 15:29:47 +0700

    ๊Update Layout

--- koomwan/app/(tabs)/suggestion/_layout.tsx
+++ koomwan/app/(tabs)/suggestion/_layout.tsx
@@ -5,7 +5,7 @@ import { Stack } from "expo-router";
 export default function SuggestionLayout() {
   return (
     //The Stack Indicate Which Main Pages And Sub-Pages Suggestion Feature Has.
-    <Stack screenOptions={{ headerShown: false }}>    
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="suggestionResult" />
     </Stack>
