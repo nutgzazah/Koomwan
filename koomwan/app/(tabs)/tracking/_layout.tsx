ce85084af593485540850a47c73d0874fc6b61e9
thidaruttd97gooddaygoodlife <thiz.codingUUtd97@gmail.com>
2025-02-14 15:29:47 +0700

    ๊Update Layout

--- koomwan/app/(tabs)/tracking/_layout.tsx
+++ koomwan/app/(tabs)/tracking/_layout.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Stack } from "expo-router";
 
-
 export default function TrackingLayout() {
   return (
     //The Stack Indicate Which Main Pages And Sub-Pages The Tracking Feature Has.
     <Stack screenOptions={{ headerShown: false }}>    
       <Stack.Screen name="index" />
       <Stack.Screen name="medicineCollected" />
+      <Stack.Screen name="medicineDetail" />
+      <Stack.Screen name="addMedicine" />
+      <Stack.Screen name="summaryTracking" />
     </Stack>
   );
 }
