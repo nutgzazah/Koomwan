5ccad4b61ac0df25235866bf0cb1cbbf66add4d6
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2025-02-26 20:29:30 +0700

    Add Forum, comment, ReportPost and add router GET GetUrlFormPath

--- server/routes/forumRoutes.js
+++ server/routes/forumRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { requireSignIn } = require('../controllers/authController')
-const { createForumPost, getAllPost, updatePost, deletePost } = require('../controllers/forumController')
+const { createForumPost, getAllPost, updatePost, deletePost, toggleLikePost, addComment, deleteComment, reportPost, removeReports, getMyForum } = require('../controllers/forumController')
 const multer = require('multer');
 
 // ตั้งค่าอัปโหลดไฟล์ (ใช้หน่วยความจำแทน disk storage)
@@ -16,12 +16,33 @@ const router = express.Router()
 //POST
 //GETALL POST || GET
 router.get('/getAllPost' , getAllPost)
+router.get('/getMyPost' ,requireSignIn, getMyForum)
+
 //CREATE POST || POST
 router.post('/createPost' ,requireSignIn, upload.single('image') , createForumPost)
+
 //UPDATE POST || PUT
 router.put('/updatePost/:postId',upload.single('image') , updatePost)
+
 //DELETE POST || DELETE
 router.delete('/deletePost/:postId',upload.single('image') , deletePost)
+
+
+//Comment
+//กดไลค์ || POST
+router.post('/like/:postId', requireSignIn, toggleLikePost);
+
+//เพิ่มคอมเม้น || POST
+router.post('/comment/:postId', requireSignIn, addComment);
+
+//ลบคอมเม้น || DELETE
+router.delete('/comment/:postId/:commentId', requireSignIn, deleteComment);
+
+//Report
+// Report a post || POST
+router.post('/report/:postId', requireSignIn, reportPost);
+// Remove All Reports || POST
+router.post('/report/removeReport/:postId', requireSignIn, removeReports);
 
 //export
 module.exports = router
