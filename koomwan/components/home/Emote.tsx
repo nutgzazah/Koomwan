377bcb4ba5ea3e7b10c4811c01ff633bd33740c1
phuparu <phumiphat.140@gmail.com>
2025-02-28 16:46:39 +0700

    cleaned emotion and bmi

--- koomwan/components/home/Emote.tsx
+++ koomwan/components/home/Emote.tsx
@@ -1,68 +1,35 @@
 import React from "react";
 import { View, Image, Text, SafeAreaView } from "react-native";
+import { getEmotionImage, getEmotionLabel } from "../../constant/emotion";
 
 type EmotionType =
+  | "laughing"
   | "happy"
-  | "angry"
+  | "neutral"
+  | "irritated"
+  | "sick"
   | "crying"
-  | "sad"
-  | "worried"
-  | "fun"
+  | "angry"
   | "none";
-
-interface EmotionData {
-  image: any;
-  label: string;
-}
 
 interface EmoteDisplayProps {
   mood: EmotionType;
 }
 
-const EMOTION_DATA: Record<EmotionType, EmotionData> = {
-  happy: {
-    image: require("../../assets/Home/emotion-happy.png"),
-    label: "วันนี้ฉันรู้สึกสดใส อารมณ์ดี\nและเต็มไปด้วยพลังบวก!",
-  },
-  angry: {
-    image: require("../../assets/Home/emotion-angry.png"),
-    label: "วันนี้ฉันรู้สึกหงุดหงิด\nและโมโหกับหลายเรื่อง",
-  },
-  crying: {
-    image: require("../../assets/Home/emotion-cried.png"),
-    label: "วันนี้ฉันรู้สึกเสียใจมาก\nจนอยากร้องไห้",
-  },
-  sad: {
-    image: require("../../assets/Home/emotion-frustrated.png"),
-    label: "วันนี้ฉันรู้สึกเศร้า\nและท้อแท้กับชีวิต",
-  },
-  worried: {
-    image: require("../../assets/Home/emotion-ill.png"),
-    label: "วันนี้ฉันรู้สึกกังวลใจ\nกับหลายสิ่งรอบตัว",
-  },
-  fun: {
-    image: require("../../assets/Home/emotion-laugh.png"),
-    label: "วันนี้ฉันรู้สึกสนุกสนาน\nและมีความสุขกับทุกอย่าง",
-  },
-  none: {
-    image: require("../../assets/Home/emotion-none.png"),
-    label: "วันนี้ยังไม่มีข้อมูลอารมณ์เลย",
-  },
-};
-
 const EmoteDisplay: React.FC<EmoteDisplayProps> = ({ mood }) => {
-  const emotionData = EMOTION_DATA[mood];
+  const emotionImage = getEmotionImage(mood);
+  const emotionLabel = getEmotionLabel(mood);
 
   return (
     <SafeAreaView>
       <View className="flex flex-row items-center p-4 bg-white justify-evenly my-8">
         <Image
-          source={emotionData.image}
+          source={emotionImage}
           className="w-24 h-24"
           resizeMode="contain"
         />
         <Text className="text-description font-regular text-center text-secondary ml-3">
-          {emotionData.label}
+          {emotionLabel}
         </Text>
       </View>
     </SafeAreaView>
