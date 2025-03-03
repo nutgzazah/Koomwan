bfae31ad140c9c06223818327bf774ce883eb723
phuparu <phumiphat.140@gmail.com>
2025-03-03 13:30:59 +0700

    Create Loading.tsx

---
+++ koomwan/global/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, Text, View, Image } from "react-native";
+
+const Loading = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-background justify-center items-center">
+      {/* <Image
+        source={require("../../assets/koomwan2.png")}
+        className="w-24 h-24 mb-4"
+      /> */}
+      <Text className="font-regular text-body mb-2">Loading...</Text>
+      <View className="w-12 h-12 border-t-4 border-b-4 border-secondary rounded-full animate-spin"></View>
+    </SafeAreaView>
+  );
+};
+
+export default Loading;
