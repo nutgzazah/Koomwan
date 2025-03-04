2afdd13a612a080c1ec3d2f2ae1458e18f6b86e6
phuparu <phumiphat.140@gmail.com>
2025-03-05 00:29:46 +0700

    Create getUserProfile, getHealthInfo

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,28 +1,38 @@
-const express = require('express')
-const { requireSignIn } = require('../controllers/authController')
-const { beginnerSetup, } = require('../controllers/userController')
-const { addRecord, updateRecord, deleteRecord, getRecord } = require('../controllers/trackingController')
-
+const express = require("express");
+const { requireSignIn } = require("../controllers/authController");
+const {
+  beginnerSetup,
+  getHealthInfo,
+  getUserProfile,
+} = require("../controllers/userController");
+const {
+  addRecord,
+  updateRecord,
+  deleteRecord,
+  getRecord,
+} = require("../controllers/trackingController");
 
 //router object
-const router = express.Router()
+const router = express.Router();
 
 //ROUTES
+// PROFILE || GET
+router.get("/profile/:userId", requireSignIn, getUserProfile);
+// HEALTHINFO || GET
+router.get("/healthinfo/:healthInfoId", requireSignIn, getHealthInfo);
 
 //BEGGINER SETUP|| POST
-router.post('/beginnerSetup' /*,requireSignIn*/, beginnerSetup)
-
-
+router.post("/beginnerSetup" /*,requireSignIn*/, beginnerSetup);
 
 //TRACKING
 //GET ROCORD || GET
-router.get('/getRecord' /*,requireSignIn*/ , getRecord)
+router.get("/getRecord/:userId", requireSignIn, getRecord);
 //ADD RECORD || POST
-router.post('/addRecord' /*,requireSignIn*/ , addRecord)
+router.post("/addRecord" /*,requireSignIn*/, addRecord);
 //UPDATE RECORD || PUT
-router.put('/updateRecord/:recordId' /*,requireSignIn*/ , updateRecord)
+router.put("/updateRecord/:recordId" /*,requireSignIn*/, updateRecord);
 //DELETE RECORD || DELETE
-router.delete('/deleteRecord/:recordId' /*,requireSignIn*/ , deleteRecord)
+router.delete("/deleteRecord/:recordId" /*,requireSignIn*/, deleteRecord);
 
 //export
-module.exports = router
+module.exports = router;
